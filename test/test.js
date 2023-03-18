74b5cae42a011d278d271630c8d220d60974220a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-18 12:06:12 -0500

    learning to test code before deployment

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import assert from "assert";
+
+describe("Testing out function", () => {
+  describe("return samson", () => {
+    it("Should equals no name", () => {
+      assert.equal("no name", "no name");
+    });
+  });
+});
